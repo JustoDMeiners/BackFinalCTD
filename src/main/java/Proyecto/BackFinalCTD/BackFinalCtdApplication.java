f42d3acd1e376509d04dcd77ed034684ee25839d
JustoDMeiners <justito_dm@hotmail.com>
2023-02-15 17:43:32 -0300

    Comiteando

---
+++ src/main/java/Proyecto/BackFinalCTD/BackFinalCtdApplication.java
@@ -0,0 +1,13 @@
+package Proyecto.BackFinalCTD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFinalCtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFinalCtdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyecto/BackFinalCTD/BackFinalCtdApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto.BackFinalCTD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFinalCtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
